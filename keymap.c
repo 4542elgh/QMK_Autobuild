9af72cbb5845cfa6f82277fe6f1977f02b1c826a
4542elgh <4542elgh@gmail.com>
2022-02-12 00:40:38 -0800

    test

--- keymap.c
+++ keymap.c
@@ -32,7 +32,6 @@ enum {
 #define NAV      MO(_NAV)
 #define SYM      MO(_SYM)
 #define NUM      MO(_NUM)
-#define FN       MO(_FN)
 #define QTY      TO(_QWERTY)
 
 /* MOD TAP */
@@ -146,7 +145,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB  , TD(TD_QQST),  KC_W, KC_E   , KC_R   , KC_T   ,                                               KC_Y  ,  KC_U  ,   KC_I ,   KC_O , KC_P    , KC_KB_MUTE,
      CTL_ESC , KC_A       ,  KC_S, KC_D   , KC_F   , KC_G   ,                                               KC_H  ,  KC_J  ,   KC_K ,   KC_L , KC_SCLN , KC_QUOTE  ,
      KC_LSFT , KC_Z       ,  KC_X, KC_C   , KC_V   , KC_B   , TD(TD_LRBRC), NUM , ScreenShot, TD(TD_LRPRN), KC_N  ,  KC_M  , KC_COMM, KC_DOT , _______ , _______   ,
-                                   FN     , KC_LGUI, KC_LALT, KC_ENT      , NAV , SYM       , _______     , KC_SPC, KC_BSPC, KC_DEL
+                                   _______, KC_LGUI, KC_LALT, KC_ENT      , NAV , SYM       , _______     , KC_SPC, KC_BSPC, KC_DEL
     ),
 
 /*
--- rules.mk
+++ rules.mk
@@ -1,7 +1,5 @@
 OLED_ENABLE = yes
 OLED_DRIVER = SSD1306   # Enables the use of OLED displays
 ENCODER_ENABLE = yes       # Enables the use of one or more encoders
-TAP_DANCE_ENABLE = yes # Allow you to tap twice for a different key
-CONSOLE_ENABLE = no # no need for this
-MOUSEKEY_ENABLE = no # no need for this
-EXTRAFLAGS += -flto # Linker optimizer
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+TAP_DANCE_ENABLE = yes
